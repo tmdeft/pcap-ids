0db3566e6260f33e2f9726d1c8aace26c8777d07
naiame-neltheri <j.in12d067@gmail.com>
2017-11-24 20:00:07 +0800

    Initialized web services & pages

---
+++ WebService/resources/views/dashboard.blade.php
@@ -0,0 +1,109 @@
+@extends('master')
+@section('title', 'dashboard')
+@section('content')
+<div class="row">
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-primary">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-comments fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">26</div>
+                                  <div>Total attacks</div>
+                              </div>
+                          </div>
+                      </div>
+                      <a href="#">
+                          <div class="panel-footer">
+                              <span class="pull-left">View Details</span>
+                              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                              <div class="clearfix"></div>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-green">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-tasks fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">12</div>
+                                  <div>New Tasks!</div>
+                              </div>
+                          </div>
+                      </div>
+                      <a href="#">
+                          <div class="panel-footer">
+                              <span class="pull-left">View Details</span>
+                              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                              <div class="clearfix"></div>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-yellow">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-shopping-cart fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">124</div>
+                                  <div>New Orders!</div>
+                              </div>
+                          </div>
+                      </div>
+                      <a href="#">
+                          <div class="panel-footer">
+                              <span class="pull-left">View Details</span>
+                              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                              <div class="clearfix"></div>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-red">
+                      <div class="panel-heading">
+                          <div class="row">
+                              <div class="col-xs-3">
+                                  <i class="fa fa-support fa-5x"></i>
+                              </div>
+                              <div class="col-xs-9 text-right">
+                                  <div class="huge">13</div>
+                                  <div>Total attacks</div>
+                              </div>
+                          </div>
+                      </div>
+                      <a href="#">
+                          <div class="panel-footer">
+                              <span class="pull-left">View Details</span>
+                              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                              <div class="clearfix"></div>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Moving Line Chart Example
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="flot-chart">
+                                <div class="flot-chart-content" id="flot-line-chart-moving"></div>
+                            </div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+              </div>
+          </div>
+@endsection
---
+++ WebService/resources/views/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="PCAP-IDS">
+    <meta name="author" content="J.IN12D067 MUST-SICT">
+  	<title>PCAP-IDS @yield('title')</title>
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="/vendor/morrisjs/morris.css" rel="stylesheet">
+    <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link hrf="/css/custom.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  </head>
+  <body>
+    @section('navbar')
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">PCAP-IDS</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/dashboard">Dashboard</a></li>
+            <li><a href="/status">NIC status</a></li>
+            <li><a href="/application">Application Layers</a></li>
+            <li><a href="/doslog">DoS Log</a></li>
+          </ul>
+        </div>
+      </nav>
+    @show
+    <div class='container'>
+      @yield('content')
+    </div>
+  </body>
+</html>
